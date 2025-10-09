49e5b8483f860005b90ed0c0936ac33d94518b0d
João Pedro Mariano <joaopedror.mariano@gmail.com>
2025-10-09 16:05:42 -0300

    Django e Rest, porém ainda não funcionando

---
+++ src/Intro/myapp/api_urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from myapp.api import ProdutoViewSet
+
+router = DefaultRouter()
+router.register(r'produtos', ProdutoViewSet, basename='produto')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('api/', include('myapp.api_urls')),  # URLs da API
+]
