b9fd6fb72b35aa6cf29aa4da9bb0d791ec2680c3
JOAO VICTOR DE CARVALHO GUIMARAES <202407095771@alunos.ibmec.edu.br>
2025-10-16 18:08:08 -0300

    fix serializer aluno

--- src/myapp/serializers.py
+++ src/myapp/serializers.py
@@ -1,31 +1,53 @@
 from rest_framework import serializers
-from myapp.models import Aluno, Professor, Vaga, Candidatura, RegistroMonitoria
+from myapp.models import Usuario, Aluno, Monitor, MonitorTEA, Professor, Vaga, Candidatura, RegistroMonitoria
+
+
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
-        fields = ['id', 'nome', 'matricula', 'email', 'telefone', 'cr_geral', 'cr_disciplina', 'curso', 'senha']
-        read_only_fields = ['id']
-        extra_kwargs = {'senha': {'write_only': True}}
+        fields = ['matricula', 'nome', 'email', 'telefone', 'senha', 'cr_geral', 'cr_disciplina', 'curso']
+        extra_kwargs = {
+            'senha': {'write_only': True}
+        }
 
-class ProfessorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Professor
-        fields = ['id', 'nome', 'matricula', 'email', 'telefone', 'senha']
-        read_only_fields = ['id']
-        extra_kwargs = {'senha': {'write_only': True}}
 
-class VagaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vaga
-        fields = ['id', 'nome', 'pre_requisitos', 'disciplina', 'status', 'prazo_inscricao']
+# class Monitor(serializers.ModelSerializer):
+#     class Meta:
 
-class CandidaturaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Candidatura
-        fields = ['id', 'aluno', 'vaga', 'documentos', 'status', 'data_candidatura']
+# class MonitorTEA(serializers.ModelSerializer):
+#     class Meta:
+#         model = MonitorTEA
+#         fields = ['salario']
+#         read_only_fields = ['id']
+#         extra_kwargs = {'senha': {'write_only': True}}
 
-class RegistroMonitoriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RegistroMonitoria
-        fields = ['id', 'aluno', 'vaga', 'horas_trabalhadas', 'data_registro', 'validacao']
+
+# class DisciplinaSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Disciplina
+#         fields = ['id', 'nome', 'codigo']
+#         read_only_fields = ['id']
+
+
+# class ProfessorSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Professor
+#         fields = ['cpf']
+#         read_only_fields = ['id']
+#         extra_kwargs = {'senha': {'write_only': True}}
+
+# class VagaSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Vaga
+#         fields = ['id', 'nome', 'pre_requisitos', 'disciplina', 'status', 'prazo_inscricao']
+
+# class CandidaturaSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Candidatura
+#         fields = ['id', 'aluno', 'vaga', 'documentos', 'status', 'data_candidatura']
+
+# class RegistroMonitoriaSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = RegistroMonitoria
+#         fields = ['id', 'aluno', 'vaga', 'horas_trabalhadas', 'data_registro', 'validacao']
