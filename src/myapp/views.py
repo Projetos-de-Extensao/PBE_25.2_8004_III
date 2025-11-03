a4fd05d2c72b984536922540ed78799f5a0d211e
joao.jo.rj@gmail.com <81653692+Grandogs@users.noreply.github.com>
2025-11-03 12:59:29 -0300

    delete metodos of Monitor

--- src/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def receita(request):
-    receitas = Receita.objects.all()
-    context = {'receitas': receitas}
-
-    return render(request, 'minha_receitas.html', context)
